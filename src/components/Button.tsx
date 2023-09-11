f52f6a4d11d2a0d025c2d4330a4a2c8f7e66b83c
sgautier-dev <sgautier.dev@gmail.com>
2023-09-11 18:22:44 +0400

    initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import clsx from 'clsx'
+
+const variantStyles = {
+  primary:
+    'bg-slate-800 font-semibold text-slate-100 hover:bg-slate-700 active:bg-slate-800 active:text-slate-100/70 dark:bg-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-700 dark:active:text-slate-100/70',
+  secondary:
+    'bg-slate-50 font-medium text-slate-900 hover:bg-slate-100 active:bg-slate-100 active:text-slate-900/60 dark:bg-slate-800/50 dark:text-slate-300 dark:hover:bg-slate-800 dark:hover:text-slate-50 dark:active:bg-slate-800/50 dark:active:text-slate-50/70',
+}
+
+type ButtonProps = {
+  variant?: keyof typeof variantStyles
+} & (
+  | (React.ComponentPropsWithoutRef<'button'> & { href?: undefined })
+  | React.ComponentPropsWithoutRef<typeof Link>
+)
+
+export function Button({
+  variant = 'primary',
+  className,
+  ...props
+}: ButtonProps) {
+  className = clsx(
+    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
+    variantStyles[variant],
+    className,
+  )
+
+  return typeof props.href === 'undefined' ? (
+    <button className={className} {...props} />
+  ) : (
+    <Link className={className} {...props} />
+  )
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+
+import { ContainerInner, ContainerOuter } from '@/components/Container'
+
+function NavLink({
+  href,
+  children,
+}: {
+  href: string
+  children: React.ReactNode
+}) {
+  return (
+    <Link
+      href={href}
+      className="transition hover:text-orange-500 dark:hover:text-orange-400"
+    >
+      {children}
+    </Link>
+  )
+}
+
+export function Footer() {
+  return (
+    <footer className="mt-32 flex-none">
+      <ContainerOuter>
+        <div className="border-t border-slate-100 pb-16 pt-10 dark:border-slate-700/40">
+          <ContainerInner>
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-slate-800 dark:text-slate-200">
+                <NavLink href="/about">About</NavLink>
+                <NavLink href="/projects">Projects</NavLink>
+                <NavLink href="/speaking">Speaking</NavLink>
+                <NavLink href="/uses">Uses</NavLink>
+              </div>
+              <p className="text-sm text-slate-400 dark:text-slate-500">
+                &copy; {new Date().getFullYear()} Sébastien Gautier. All rights
+                reserved.
+              </p>
+            </div>
+          </ContainerInner>
+        </div>
+      </ContainerOuter>
+    </footer>
+  )
+}
