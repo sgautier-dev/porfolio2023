a61b8852d97a2885c58b2690507639e3b4e639f3
sgautier-dev <sgautier.dev@gmail.com>
2023-09-26 15:42:22 +0400

    allowedOrigins and rate limit added to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,55 +1,56 @@
-import { NextResponse } from "next/server";
-import rateLimit from "express-rate-limit";
+import { NextResponse, NextRequest } from 'next/server'
+import rateLimit from 'express-rate-limit'
 
-const allowedOrigins =
-	process.env.NODE_ENV === "production"
-		? ["https://sgautier.dev", "https://www.sgautier.dev"]
-		: ["http://localhost:3000"];
+const allowedOrigins = new Set(
+  process.env.NODE_ENV === 'production'
+    ? ['https://sgautier.dev', 'https://www.sgautier.dev']
+    : ['http://localhost:3000'],
+)
 
 const getKey = (req: Request) => {
-	return req.headers.get("x-forwarded-for") || "unknown-ip";
-};
+  return req.headers.get('x-forwarded-for') || 'unknown-ip'
+}
 
 const rateLimiter = rateLimit({
-	keyGenerator: getKey,
-	windowMs: 60 * 1000,
-	max: 10,
-});
+  keyGenerator: getKey,
+  windowMs: 60 * 1000,
+  max: 10,
+})
 
-export async function middleware(request: Request) {
-	// Check allowed origins
-	const origin = request.headers.get("origin");
-	if (origin && !allowedOrigins.includes(origin)) {
-		return new NextResponse(null, {
-			status: 403,
-			statusText: "Forbidden",
-			headers: {
-				"Content-Type": "text/plain",
-				"Access-Control-Allow-Origin": allowedOrigins.join(", "),
-			},
-		});
-	}
+export async function middleware(request: NextRequest) {
+  // Check allowed origins
+  const origin = request.headers.get('origin')
+  if (origin && !allowedOrigins.has(origin)) {
+    return new NextResponse(null, {
+      status: 403,
+      statusText: 'Forbidden',
+      headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': Array.from(allowedOrigins).join(", "),
+      },
+    })
+  }
 
-	// Apply rate limiting
-	let rateLimited: boolean = false;
-	rateLimiter(request as any, {} as any, () => {
-		rateLimited = true;
-	});
+  // Apply rate limiting
+  let rateLimited: boolean = false
+  rateLimiter(request as any, {} as any, () => {
+    rateLimited = true
+  })
 
-	if (rateLimited) {
-		return new NextResponse(null, {
-			status: 429,
-			statusText: "Too Many Requests",
-			headers: {
-				"Retry-After": "60",
-				"Content-Type": "text/plain",
-			},
-		});
-	}
+  if (rateLimited) {
+    return new NextResponse(null, {
+      status: 429,
+      statusText: 'Too Many Requests',
+      headers: {
+        'Retry-After': '60',
+        'Content-Type': 'text/plain',
+      },
+    })
+  }
 
-	return null;
+  return null
 }
 
 export const config = {
-	matcher: "/api/:path*",
-};
+  matcher: '/api/:path*',
+}
